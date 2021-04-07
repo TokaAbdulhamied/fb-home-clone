3ff6772f0f99f9842863b1e770d399a3a2ed175d
Toka Abdulhamied <tokaabdulhamied@gmail.com>
2021-04-08 01:13:33 +0200

    add login component and connect it  with firebase

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import { styled } from '@material-ui/core';
+import React, {useState} from 'react'
+import {auth, provider} from '../../firebase'
 import './Login.css';
 function Login() {
+  const signHandler =()=>{
+    // import auth and provider from local firebase 
+    auth.signInWithPopup (provider)
+    .then ((respose)=> {
+      console.log(respose)
+    })
+    .catch ((error) => alert (error.message))
+    
+  }
+
   return (
     <div className="login">
       <div className="login__logo">
         <img src="https://www.elzahrawy.com/wp-content/uploads/2021/01/facebook_logos_PNG19754.png" alt="logo"/>
       </div>
       <h1>facebook</h1>
-      <button type="submit" >
+      <button type="submit" onClick={signHandler} >
         Sign in
       </button>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase"
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCZayfc-sGgzSlAERUBEDmJYWhQD3J1qXg",
     authDomain: "fb-clone-415fe.firebaseapp.com",
@@ -6,3 +9,11 @@ const firebaseConfig = {
     messagingSenderId: "528081881542",
     appId: "1:528081881542:web:5b0bd7e8e3efc9d2c28ac7"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig) 
+const db = firebaseApp.firestore(); 
+export const auth = firebase.auth(); 
+// supporting google login
+export const provider =  new firebase.auth.GoogleAuthProvider(); 
+
+export default db 
